b3a3e57d789a7e2918ffa3449ed3fc0c0da5e51a
ravasa01 <sandeep.ravali.ap@nielsen.com>
2017-08-30 20:35:47 +0530

    Assignment 1.2

---
+++ src/assignment1_2/Assignment1_2.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package assignment1_2;
+
+/**
+ * This class will add two numbers and print their output
+ *
+ */
+public class Assignment1_2 {
+
+	public static void main(String[] args) {// Main method starts
+		
+		int number1;
+		int number2;
+		int sumOfNumbers;// Declaring two numbers and its sum as integers
+
+		number1=100;
+		number2=90; // Assigning values to variables
+		
+		sumOfNumbers=number1-(-number2); // Calculating sum using operator other than +
+		
+		System.out.println("Sum of the numbers "+number1+" and "+number2+" is :"+sumOfNumbers); // Printing output of sum of two numbers
+		
+		}
+
+} // class closed
